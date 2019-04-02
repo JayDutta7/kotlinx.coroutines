faf288ad9d497072395d4a45c3427676ad1464af
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2019-04-02 12:50:55 +0300

    Copy all flow builders to Builders.kt

--- kotlinx-coroutines-core/common/src/flow/Builders.kt
+++ kotlinx-coroutines-core/common/src/flow/Builders.kt
@@ -2,16 +2,15 @@
  * Copyright 2016-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
-@file:UseExperimental(ExperimentalTypeInference::class)
 @file:JvmMultifileClass
 @file:JvmName("FlowKt")
 
 package kotlinx.coroutines.flow
 
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
 import kotlinx.coroutines.flow.internal.*
 import kotlin.coroutines.*
-import kotlin.experimental.*
 import kotlin.jvm.*
 
 /**
@@ -97,7 +96,6 @@ public fun <T : Any> Iterator<T>.asFlow(): Flow<T> = unsafeFlow {
     }
 }
 
-
 /**
  * Creates flow that produces values from the given sequence.
  */
@@ -115,3 +113,86 @@ public fun <T : Any> flowOf(vararg elements: T): Flow<T> = unsafeFlow {
         emit(element)
     }
 }
+
+/**
+ * Creates flow that produces values from the given array.
+ */
+public fun <T: Any> Array<T>.asFlow(): Flow<T> = flow {
+    forEach { value ->
+        emit(value)
+    }
+}
+
+/**
+ * Creates flow that produces values from the given array.
+ */
+public fun IntArray.asFlow(): Flow<Int> = flow {
+    forEach { value ->
+        emit(value)
+    }
+}
+
+/**
+ * Creates flow that produces values from the given array.
+ */
+public fun LongArray.asFlow(): Flow<Long> = flow {
+    forEach { value ->
+        emit(value)
+    }
+}
+
+/**
+ * Creates flow that produces values from the given range.
+ */
+public fun IntRange.asFlow(): Flow<Int> = flow {
+    forEach { value ->
+        emit(value)
+    }
+}
+
+/**
+ * Creates flow that produces values from the given range.
+ */
+public fun LongRange.asFlow(): Flow<Long> = flow {
+    forEach { value ->
+        emit(value)
+    }
+}
+
+/**
+ * Creates an instance of the cold [Flow] from a supplied [SendChannel].
+ *
+ * To control backpressure, [bufferSize] is used and matches directly the `capacity` parameter of [Channel] factory.
+ * The provided channel can later be used by any external service to communicate with flow and its buffer determines
+ * backpressure buffer size or its behaviour (e.g. in case when [Channel.CONFLATED] was used).
+ *
+ * Example of usage:
+ * ```
+ * fun flowFrom(api: CallbackBasedApi): Flow<Int> = flowViaChannel { channel ->
+ *     val adapter = FlowSinkAdapter(channel) // implementation of callback interface
+ *     api.register(adapter)
+ *     channel.invokeOnClose {
+ *         api.unregister(adapter)
+ *     }
+ * }
+ * ```
+ */
+public fun <T : Any> flowViaChannel( // TODO bikeshed this naming?
+    bufferSize: Int = 16,
+    @BuilderInference block: suspend (SendChannel<T>) -> Unit
+): Flow<T> {
+    require(bufferSize >= 0) { "Buffer size should be positive, but was $bufferSize" }
+    return flow {
+        coroutineScope {
+            val channel = Channel<T>(bufferSize)
+            launch {
+                block(channel)
+            }
+
+            channel.consumeEach { value ->
+                emit(value)
+            }
+        }
+    }
+}
+
